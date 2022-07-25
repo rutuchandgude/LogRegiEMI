7e2dc53f4fc002299e3a89e77d6afceab2ded8c4
Rutuja Chandgude <74643325+rutuchandgude@users.noreply.github.com>
2022-07-25 10:15:22 +0530

    Update Homepage.js

--- EMIcal/emical/src/components/homepage/Homepage.js
+++ EMIcal/emical/src/components/homepage/Homepage.js
@@ -2,11 +2,11 @@ import React,{useState} from "react"
 import "./Homepage.css"
 import {withStyles} from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
-// import { Typography } from "@material-ui/core/Typography";
 import {Table,TableCell,TableRow} from '@material-ui/core';
 import {Pie} from 'react-chartjs-2';
 import TableDetails from "../TableDetails"; 
 import SlideMarks from "../../SlideMark";
+
 
 const PrettoSlider = withStyles({
     root : {color:'MediumVioletRed',height:10},
@@ -18,51 +18,77 @@ const PrettoSlider = withStyles({
 const Homepage = ({setLoginUser}) =>{
 
 
-    const [pAmount,setpAmount] = useState(2755000);
-    const [interest,setinterest] = useState(7);
-    const[duration,setDuration] = useState(147);
+
+    const [int,setInt] =useState(
+        {
+            loanAmount:2755000,
+            interest : 7, 
+            duration : 147,
+        }
+    )
+
+   
     const maxValue = 6000000;
     const intMax = 20;
     const maxDuration = 360;
 
-    const intr = interest/1200;
-    const emi = duration ? Math.round(pAmount * intr /(1-(Math.pow(1/(1+intr),duration)))) : 0;
-    const totalAmt = duration * emi;
-    var TotalAmountOfCredit = Math.round((emi/intr) * (1- Math.pow((1+intr), (-duration))));
+  
+    const intr = int.interest/1200;
+    const emi = int.duration ? Math.round(int.loanAmount * intr /(1-(Math.pow(1/(1+intr),int.duration)))) : 0;
+    const totalAmt = int.duration * emi;
+    var TotalAmountOfCredit = Math.round((emi/intr) * (1- Math.pow((1+intr), (-int.duration))));
     const TotalAmountOfInterest = Math.round(totalAmt - TotalAmountOfCredit);
 
-    const [int,setInt] =useState({loanAmount:pAmount})
-    return(
+    
+    return( 
         <>
         <div className="homepage">
+            
             <div className="App">
+            
                 <div className="CalApp">
                   <h2 className="CalHandling"><u>EMI Calculator</u></h2>
                   <div>
                   <h4><strong>Loan Amount</strong></h4>    
-                  <PrettoSlider value={pAmount} marks={SlideMarks.marksAmt} onChange={(event,vAmt)=>{setpAmount(vAmt)}} defaultValue={pAmount} max ={maxValue}></PrettoSlider>
+                  <PrettoSlider value={int.loanAmount} marks={SlideMarks.marksAmt} onChange={(event,vAmt)=>{
+                        setInt({
+                            ...int,
+                           "loanAmount":vAmt
+                        })
+                    }} defaultValue={int.loanAmount} max ={maxValue}></PrettoSlider>
                   </div>
                   <div>
-                    <h4><strong>Interest Rate%</strong></h4>
-                    <PrettoSlider value={interest} marks={SlideMarks.marksInt} onChange={(event,vInt)=>{setinterest(vInt);}} max={intMax} defaultValue={interest}></PrettoSlider>
+                    <h4><strong>Interest Rate</strong></h4>
+                    <PrettoSlider value={int.interest} marks={SlideMarks.marksInt} onChange={(event,vInt)=>{
+                          setInt({
+                            ...int,
+                           "interest":vInt
+                        })
+                    } } max={intMax} defaultValue={int.interest}></PrettoSlider>
                   </div>
                   <div>
                     <h4><strong>Tenure (Months)</strong></h4>
-                    <PrettoSlider value={duration} marks={SlideMarks.marksTenure} onChange={(event,vDur) =>{setDuration(vDur);}} max={maxDuration} defaultValue={duration}></PrettoSlider>
+                    <PrettoSlider value={int.duration} marks={SlideMarks.marksTenure} onChange={(event,vDur) =>{
+                          setInt({
+                            ...int,
+                           "duration":vDur
+                        })
+                    }} max={maxDuration} defaultValue={int.duration}></PrettoSlider>
                   </div>
                  
                 
                <Table>
+            
                 <TableRow>
                 <TableCell>
-                    <TableDetails int={int} setInt={setInt} pAmount={pAmount} totalAmt={totalAmt} interest={interest} duration={duration} emi={emi} TotalAmountOfInterest={TotalAmountOfInterest}></TableDetails>
+                    <TableDetails int={int} setInt={setInt} loanAmount={int.loanAmount} interest={int.interest} duration={int.duration} emi={emi} TotalAmountOfInterest={TotalAmountOfInterest} totalAmt={totalAmt}></TableDetails>
                 </TableCell>
                 <TableCell>
                     <Pie 
                      data = {{
-                        labels : ['Total Interest','Total Amount'],
+                        labels : ['Total interest','Total Amount'],
                         datasets : [{
-                            data : [TotalAmountOfInterest,pAmount],
+                            data : [TotalAmountOfInterest,int.loanAmount],
                             backgroundColor : ['red','green']
                         }]
                      }}
@@ -72,12 +98,13 @@ const Homepage = ({setLoginUser}) =>{
                 </TableCell>
                 </TableRow>
                </Table>
+               
                <div className="button" onClick={() => setLoginUser({})}>Logout</div>
             </div>
             </div>
-           
+                    
         </div>
-        
+      
         </>
     )
 }
